9ce7660e9f5a64e72b7b312e8ccf4e4b0b00c0d6
Iraitz Compains <iracom82@gmail.com>
2014-04-22 12:54:43 +0200

    Se ha añadido un ejercicio que pide introducir 5 números y devuelve el máximo y el mínimo.

---
+++ EjerciciosJava/src/com/iraitzcompains/arrays/Ej16_01.java
@@ -0,0 +1,34 @@
+package com.iraitzcompains.arrays;
+
+import java.util.Scanner;
+
+public class Ej16_01 {
+
+	public static void main(String[] args) {
+		int numeros[] = new int[5];
+		int max, min;
+		
+		for (int i = 0; i < numeros.length; i++)
+		{
+			System.out.println("Introduzca un nœmero entero:");
+			Scanner entrada = new Scanner(System.in);
+			numeros[i] = entrada.nextInt();
+		}
+		
+		max = numeros[0];
+		min = numeros[0];
+		for(int j = 1; j < numeros.length; j++)
+		{
+			if(numeros[j] > max)
+			{
+				max = numeros[j];
+			}
+			if(numeros[j] < min)
+			{
+				min = numeros[j];
+			}
+		}
+		System.out.println ("El nœmero m‡ximo es: " + max + " y el nœmero m’nimo es: " + min);
+	}
+
+}
